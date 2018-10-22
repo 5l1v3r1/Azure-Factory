33d83c1903046f0263c00c8edd7a2f17977ef561
Kaz <kaz@innvtv.com>
2018-10-22 16:58:02 -0700

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
